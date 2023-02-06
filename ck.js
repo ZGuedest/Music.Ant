c092be35de6030ad0182b81b54ef22a8e61b9b16
may <mayjuanbaticono@gmail.com>
2023-02-07 00:15:23 +0100

    crear hoja login

--- ck.js
+++ ck.js
@@ -1,4 +1,18 @@
-let ck_primerradio = document.getElementById(firstRadio);
-let ck_segundoradio = document.getElementById(secondRadio);
-let ck_tercerradio = document.getElementById(thirdRadio);
+let ck_primerradio = document.getElementById(ck_firstRadio);
+let ck_segundoradio = document.getElementById(ck_secondRadio);
+let ck_tercerradio = document.getElementById(ck_thirdRadio);
 
+function gastosDeEnvio()
+
+if (document.getElementById('ck_firstRadio').checked)
+{
+alert('gratis');
+}
+if (document.getElementById('ck_secondRadio').checked)
+{
+alert('10€ gastos de envio');
+}
+if (document.getElementById('ck_thirdRadio').checked)
+{
+alert('20€ gastos de envio');
+}
