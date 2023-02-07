de2835e4602b7e623af1d9516057c268c47867c0
Zguedest <zucelys.zge@gmail.com>
2023-02-07 10:45:57 +0100

    primer objeto de guitarra

--- zucelys.js
+++ zucelys.js
@@ -1,1 +1,27 @@
-let guitarras [];
+var guitarra1 = {
+
+    "id":1,
+    "name": "Yamaha CGS103A Classical Guitar",
+    "description": "Tapa de pícea. Aros y fondo de nato.Mástil de nato.Diapasón y puente de palisandro (Dalbergia latifolia).Profundidad del cuerpo: 87 - 91mm. Escala: 575mm. Ancho de la cejilla: 43mm. Herrajes cromados. Acabado: Natural", 
+    "precioDescription": "138€",
+    "precio": 138,
+    "src": "./img/guitarra1.jpg"
+};
+
+var guitarra2 = {
+
+    "id":1,
+    "name": "Guitarra clásica",
+    "description": 'Ford',
+    "precio": 'Mustang',
+    "src": "./img/guitarra1.jpg"
+};
+
+var guitarra1 = {
+
+    "id":1,
+    "name": "Guitarra clásica",
+    "description": 'Ford',
+    "precio": 'Mustang',
+    "src": "./img/guitarra1.jpg"
+};
