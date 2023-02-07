da044455033fd2df1a5ee1b6c8595015b8aaf290
Zguedest <zucelys.zge@gmail.com>
2023-02-07 10:57:02 +0100

    objeto guitarra 3

--- zucelys.js
+++ zucelys.js
@@ -18,12 +18,12 @@ var guitarra2 = {
     "src": "./img/guitarra2.jpg"
 };
 
-var guitarra1 = {
+var guitarra3 = {
 
     "id":3,
-    "name": "Yamaha CGS103A Classical Guitar",
-    "description": "Tapa de pícea. Aros y fondo de nato.Mástil de nato.Diapasón y puente de palisandro (Dalbergia latifolia).Profundidad del cuerpo: 87 - 91mm. Escala: 575mm. Ancho de la cejilla: 43mm. Herrajes cromados. Acabado: Natural", 
-    "precioDescription": "138€",
-    "precio": 138,
-    "src": "./img/guitarra1.jpg"
+    "name": "Schecter Diamond Omen 7 Extreme BCH Guitarra Eléctrica",
+    "description": "Cuerpo de caoba. Tapa de arce flameado. Mástil de arce. Diapasón de palisandro Griffbrett (Dalbergia latifolia), 24 trastes. Incrustaciones Vector. Medida: 26,5 (673 mm). Puente TOM. Paso de cuerdas a través del cuerpo, 2 pastillas Schecter Diamond Plus Humbucker. Color: Black Cherry", 
+    "precioDescription": "629 €",
+    "precio": 629,
+    "src": "./img/guitarra3.jpg"
 };
