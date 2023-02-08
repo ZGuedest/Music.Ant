8322c637bed535a2455b1b21d33098b578d786e0
bzmay0149 <mayjuanbaticono@gmail.com>
2023-02-08 15:05:53 +0100

    funcion gastos de envio

--- ck.js
+++ ck.js
@@ -1,18 +1,44 @@
-let ck_primerradio = document.getElementById(ck_firstRadio);
-let ck_segundoradio = document.getElementById(ck_secondRadio);
-let ck_tercerradio = document.getElementById(ck_thirdRadio);
+let ck_primerradio = document.getElementById("ck_firstRadio");
+let ck_segundoradio = document.getElementById("ck_secondRadio");
+let ck_tercerradio = document.getElementById("ck_thirdRadio");
+let gastosE= document.getElementById("ck_ge");
 
-function gastosDeEnvio()
+ck_primerradio.addEventListener("change",()=>{
+    gastosDeEnvio()
+})
 
-if (document.getElementById('ck_firstRadio').checked)
-{
-alert('gratis');
+ck_segundoradio.addEventListener("change",()=>{
+    gastosDeEnvio()
+})
+
+ck_tercerradio.addEventListener("change",()=>{
+    gastosDeEnvio()
+    
+})
+
+function gastosDeEnvio(){
+
+        if (document.getElementById('ck_firstRadio').checked)
+        {
+            gastosE.innerHTML="0€"
+            gastosE.value= parseFloat(0)
+        }
+        else if (document.getElementById('ck_secondRadio').checked)
+        {
+            gastosE.innerHTML="10€"
+            gastosE.value= parseFloat(10)
+            
+        }
+        else
+        {
+            gastosE.innerHTML="20€"
+            gastosE.value= parseFloat(20)
+
+        }
+
+
 }
-if (document.getElementById('ck_secondRadio').checked)
-{
-alert('10€ gastos de envio');
-}
-if (document.getElementById('ck_thirdRadio').checked)
-{
-alert('20€ gastos de envio');
-}
+
+
+
+
