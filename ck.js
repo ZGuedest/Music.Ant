73559fe044b2a6bf2e95d6ac786dee224ce8e9d1
bzmay0149 <mayjuanbaticono@gmail.com>
2023-02-09 12:32:40 +0100

    cupon de descuento y total de productos

--- ck.js
+++ ck.js
@@ -2,52 +2,112 @@ let ck_primerradio = document.getElementById("ck_firstRadio");
 let ck_segundoradio = document.getElementById("ck_secondRadio");
 let ck_tercerradio = document.getElementById("ck_thirdRadio");
 let gastosE = document.getElementById("ck_ge");
-let totatP = document.getElementById("ck_producto").value;
-let descuento = document.getElementById("ck_dto").value;
+let totatP = document.getElementById("ck_producto");
+let descuento = document.getElementById("ck_dto");
 let total = document.getElementById("ck_total");
-total = gastosE + totalP - descuento;
+
+let cupon = document.getElementById("inputcupon");
+
+
+let codigoReal= "250X"
+
+
+// let btnPedido = document.getElementById("ck_bntpedido");
+
+
+
+
+
+gastosDeEnvio()
+
+totalGastos()
 
 
 
 ck_primerradio.addEventListener("change",()=>{
     gastosDeEnvio()
+    totalGastos()
+
 })
 
 ck_segundoradio.addEventListener("change",()=>{
     gastosDeEnvio()
+    totalGastos()
 })
 
 ck_tercerradio.addEventListener("change",()=>{
     gastosDeEnvio()
+    totalGastos()
 
 })
 
-function gastosDeEnvio(){
+cupon.addEventListener("keyup",(event)=>{
+    debugger
+    if (event.code === 'Enter') {
+        calcular()
+        //totalGastos()
+    }
 
+})
+
+
+//FUNCIONESSSS
+function gastosDeEnvio(){
         if (document.getElementById('ck_firstRadio').checked)
         {
-            gastosE.innerHTML="0€"
+            gastosE.innerHTML="0"
             gastosE.value= parseFloat(0)
         }
         else if (document.getElementById('ck_secondRadio').checked)
         {
-            gastosE.innerHTML="10€"
+            gastosE.innerHTML="10"
             gastosE.value= parseFloat(10)
             
         }
         else
         {
-            gastosE.innerHTML="20€"
+            gastosE.innerHTML="20"
             gastosE.value= parseFloat(20)
 
         }
-
-
 }
-//function totalGastos(){
-    //total.innerHTML = "gastorE" + "totalP.value" - "descuento"
-     
-//}
+
+
+  function totalGastos(){
+
+      //sumar y restar al total
+      let totalPagar = parseFloat(gastosE.value)+parseFloat (totatP.getAttribute("value"))-parseFloat(descuento.getAttribute("value"));
+      total.innerHTML = totalPagar
+      total.value = totalPagar; 
+  }
+
+  //aplicar descuento
+  function calcular(){
+
+    console.log(cupon.textContent)
+
+    if(cupon.value==codigoReal){
+        let dto = 0.21;
+        let suma = parseFloat(totatP.getAttribute("value"));
+        let dtoT = suma * dto;
+        descuento.innerHTML = dtoT;
+        descuento.setAttribute("value",dtoT)
+        console.log(descuento.getAttribute("value"))
+        let dtoTotal = totatP.getAttribute("value") - dtoT;
+    
+        total.setAttribute("value",dtoTotal)
+        total.innerHTML= dtoTotal
+    }else{
+        alert("Código inexistente")
+    }
+
+
+
+  }
+
+
+
+
 
 
 
