11f85a9ced36129f43e13facef661301e67f36c3
Zguedest <zucelys.zge@gmail.com>
2023-02-09 10:16:45 +0100

    menu de categorias del catalogo

--- catalogo.js
+++ catalogo.js
@@ -1,8 +1,60 @@
+const radioCatalogo = document.getElementById('radiocatalogo');
+
+const radioCuerda = document.getElementById('radio-cuerda');
+const checksCuerda = document.getElementById('checks-cuerda');
+
+const radioViento = document.getElementById('radio-viento');
+const checksViento = document.getElementById('checks-viento');
+
+const radioPercusion = document.getElementById('radio-percusion');
+const checksPercusion = document.getElementById('checks-percusion');
+
+const radioElectronico = document.getElementById('radio-electronico');
+const checksElectronico = document.getElementById('checks-electronico');
+
+yesnoCheck(); 
+
+radioCuerda.addEventListener('change', (event)=>{
+    yesnoCheck()
+});
+
+
+radioPercusion.addEventListener('change',  (event)=>{
+    yesnoCheck()
+});
+
+
+radioViento.addEventListener('change', yesnoCheck)
+radioElectronico.addEventListener('change', yesnoCheck)
+
+
+
+
 function yesnoCheck() {
-    if (document.getElementById('radio-cuerda').checked) {
-        document.getElementById('cheks-cuerda').style.display = 'flex';
-    } else {
-        document.getElementById('cheks-cuerda').style.visibility = 'none';
+    debugger
+    if (radioCuerda.checked==true) {
+        checksCuerda.style.display = 'block';
+        checksViento.style.display = 'none';
+        checksPercusion.style.display = 'none';
+        checksElectronico.style.display = 'none';
+    } else if(radioViento.checked==true){ 
+        checksCuerda.style.display = 'none';
+        checksViento.style.display = 'block';
+        checksPercusion.style.display = 'none';
+        checksElectronico.style.display = 'none';
+
+    }else if(radioPercusion.checked==true){
+        checksCuerda.style.display = 'none';
+        checksViento.style.display = 'none';
+        checksPercusion.style.display = 'block';
+        checksElectronico.style.display = 'none';
+
+    }else if(radioElectronico.checked==true){
+        checksCuerda.style.display = 'none';
+        checksViento.style.display = 'none';
+        checksPercusion.style.display = 'none';
+        checksElectronico.style.display = 'block';
+
     }
 
 }
