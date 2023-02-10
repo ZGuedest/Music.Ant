97c2cc28acc72bd8e27ab13dc05054331d180883
Medibra322 <medibra322@gmail.com>
2023-02-10 12:28:32 +0100

    login

---
+++ login.js
@@ -0,0 +1,23 @@
+let boton =document.getElementById("button");
+let gmail =document.getElementById("form2Example11");
+let password=document.getElementById("form2Example22");
+
+
+boton.addEventListener('click',regestrar);
+function regestrar(){
+
+    let gamil_gmail=gmail.value;
+    let password_password=password.value;
+
+    localStorage.setItem('gmail',gamil_gmail);
+    localStorage.setItem('password',password_password);
+
+
+
+
+
+
+}
+
+
+
