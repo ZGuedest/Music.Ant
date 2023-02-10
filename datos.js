b40ca31ff6e979915ccd7582e4bc6cd2fedb0f77
Zguedest <zucelys.zge@gmail.com>
2023-02-10 18:45:57 +0100

    modificando la bd

--- datos.js
+++ datos.js
@@ -1,22 +1,30 @@
-var guitarra1 = {
+const productos=[
+    {
 
-    "id":1,
-    "name": "Yamaha CGS103A Guitarra Clásica",
-    "description": "Tapa de pícea. Aros y fondo de nato.Mástil de nato.Diapasón y puente de palisandro (Dalbergia latifolia).Profundidad del cuerpo: 87 - 91mm. Escala: 575mm. Ancho de la cejilla: 43mm. Herrajes cromados. Acabado: Natural", 
-    "precioDescription": "138€",
-    "precio": 138,
-    "src": "./img/guitarra1.jpg"
-};
+        "id":1,
+        "varname":"guitarra1",
+        "name": "Yamaha CGS103A Guitarra Clásica",
+        "description": "Tapa de pícea. Aros y fondo de nato.Mástil de nato.Diapasón y puente de palisandro (Dalbergia latifolia).Profundidad del cuerpo: 87 - 91mm. Escala: 575mm. Ancho de la cejilla: 43mm. Herrajes cromados. Acabado: Natural", 
+        "precioDescription": "138€",
+        "precio": 138,
+        "src": "./img/guitarra1.jpg"
+    },
 
-var guitarra2 = {
+    {
 
-    "id":2,
-    "name": "Ibanez GRG121DX-BKF - Guitarra Eléctrica",
-    "description": "Cuerpo de álamo. Mástil de arce GRG .Diapasón de corazón púrpura. Inlays de 'Dientes de tiburón', 24 trastes Jumbo, 2 pastillas humbucker IBZ-6. Puente fijo. Herrajes negros. Acabado: Negro liso", 
-    "precioDescription": "238 €",
-    "precio": 238,
-    "src": "./img/guitarra2.jpg"
-};
+        "id":2,
+        "varname":"guitarra2",
+        "name": "Ibanez GRG121DX-BKF - Guitarra Eléctrica",
+        "description": "Cuerpo de álamo. Mástil de arce GRG .Diapasón de corazón púrpura. Inlays de 'Dientes de tiburón', 24 trastes Jumbo, 2 pastillas humbucker IBZ-6. Puente fijo. Herrajes negros. Acabado: Negro liso", 
+        "precioDescription": "238 €",
+        "precio": 238,
+        "src": "./img/guitarra2.jpg"
+    },
+
+
+]
+
+
 
 var guitarra3 = {
 
