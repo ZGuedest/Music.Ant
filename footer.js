143401941e0938e60cb258f6c94e486e1339a9c5
Zguedest <zucelys.zge@gmail.com>
2023-02-14 09:22:20 +0100

    uniendo menu y footer dinamico

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-let array = ["Síguenos en nuestras redes sociales", "Suscríbete para recibir actualizaciones", "Subscribe", "Política de Cookies", "Política de Privacidad", "Aviso Legal", "Política de Cookies", "Política de Privacidad", "Aviso Legal", "Descárgate Nuestra App", "Contáctanos", "New York, NY 10012, US", "info@example.com ", "+ 01 234 567 88", "© 2023 Copyright:", "MusicAnt.com"] 
+let arrayF = ["Síguenos en nuestras redes sociales", "Suscríbete para recibir actualizaciones", "Subscribe", "Política de Cookies", "Política de Privacidad", "Aviso Legal", "Política de Cookies", "Política de Privacidad", "Aviso Legal", "Descárgate Nuestra App", "Contáctanos", "New York, NY 10012, US", "info@example.com ", "+ 01 234 567 88", "© 2023 Copyright:", "MusicAnt.com"] 
 let arrayIngles =["Follow us on our social networks", "Subscribe to receive updates", "Subscribete", "Cookies policy", "Legal warning", , "Cookies policy", "Privacy Policy", "Legal warning", "Download Our App", "Contact us", "New York, NY 10012, US", "info@example.com ", "+ 01 234 567 88", "© 2023 Copyright:", "MusicAnt.com"]
 let arrayeuskera = ["Jarrai gaitzazu gure sare sozialetan", "Harpidetu eguneraketak jasotzeko",  "harpidetu", "cookien politika", 
 "Pribatutasun politika", "Lege abisua", "Deskargatu Gure aplikazioa", "jarri gurekin harremanetan" , "New York, NY 10012, US","© 2023 Copyright:","MusicAnt.com"]
@@ -7,7 +7,7 @@ debugger
 let html= document.getElementsByTagName("html")[0]
 let footer = document.createElement("footer")
 footer.className="text-center text-white"
-footer.innerHTML=`<h2>${array[0]}</h2>
+footer.innerHTML=`<h2>${arrayF[0]}</h2>
 <!-- Grid container -->
 <div class="container p-4">
     <!-- Section: Social media -->
@@ -41,7 +41,7 @@ footer.innerHTML=`<h2>${array[0]}</h2>
         <!--Grid column-->
         <div class="col-auto">
             <p class="pt-2">
-            <strong>${array[1]}</strong>
+            <strong>${arrayF[1]}</strong>
             </p>
         </div>
         
@@ -59,7 +59,7 @@ footer.innerHTML=`<h2>${array[0]}</h2>
         <div class="col-auto">
             <!-- Submit button -->
             <button type="submit" class="btn btn-outline-light mb-4">
-            ${array[2]}
+            ${arrayF[2]}
             </button>
         </div>
         <!--Grid column-->
@@ -84,18 +84,18 @@ footer.innerHTML=`<h2>${array[0]}</h2>
          <!--Grid column-->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-4">
             <div class="d-flex flex-colum flex-wrap">
-                <a class="text-white col-12 text-md-start" href="">${array[3]}</a>
-                <a class="text-white col-12 text-md-start" href="">${array[4]}</a>
-                <a class="text-white col-12 text-md-start" href="">${array[5]}</a>
-                <a class="text-white col-12 text-md-start" href="">${array[6]}</a>
-                <a class="text-white col-12 text-md-start" href="">${array[7]}</a>
-                <a class="text-white col-12 text-md-start" href="">${array[8]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[3]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[4]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[5]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[6]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[7]}</a>
+                <a class="text-white col-12 text-md-start" href="">${arrayF[8]}</a>
             </div>
         </div>
         <!--Grid column-->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0">
             <!-- Links -->
-            <h6 class="text-uppercase fw-bold mt-4">${array[9]}</h6>
+            <h6 class="text-uppercase fw-bold mt-4">${arrayF[9]}</h6>
             <div class="d-flex flex-row flex-wrap justify-content-around" style="width: 100%;">
                 <div id="appel-store"></div>
                 <div id="play-store"></div>
@@ -106,11 +106,11 @@ footer.innerHTML=`<h2>${array[0]}</h2>
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-4">
             <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">${array[10]}</h6>
+            <h6 class="text-uppercase fw-bold mb-4">${arrayF[10]}</h6>
             
             <div class="d-flex flex-colum flex-wrap">
-                <p class="col-12 text-md-start"><i class="fas fa-home me-3"></i>${array[11]}</p>
-                <p class="col-12 text-md-start"><i class="fas fa-envelope me-3"></i>${array[12]}</p>
+                <p class="col-12 text-md-start"><i class="fas fa-home me-3"></i>${arrayF[11]}</p>
+                <p class="col-12 text-md-start"><i class="fas fa-envelope me-3"></i>${arrayF[12]}</p>
                 <p class="col-12 text-md-start"><i class="fas fa-phone me-3"></i>+ 01 234 567 88</p>
             </div>
             
@@ -125,8 +125,8 @@ footer.innerHTML=`<h2>${array[0]}</h2>
 
 <!-- Copyright -->
 <div id="copyright" class="text-center p-3">
-${array[13]}
-    <a class="text-white" href="">${array[14]}</a>
+${arrayF[13]}
+    <a class="text-white" href="">${arrayF[14]}</a>
 </div>
 <!-- Copyright -->
 `
--- menu.js
+++ menu.js
@@ -1,35 +1,4 @@
-{/* <header>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-xl">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        <div id="logo"></div>
-        
-        <div id="navbarNav" class="collapse navbar-collapse justify-content-around" >
-            <input id="buscador" class="form-control" type="search" placeholder="Buscar" aria-label="Buscar">
-            <ul id="menu-interior"class="navbar-nav justify-content-around">
-                <li class="nav-item">
-                    <a class="nav-link" href="catalogo.html">Catálogo</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#novedades">Novedades</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#ofertas">Ofertas</a>
-                </li>
-            </ul>
-        </div>
-
-        
-        <ul id="menu-iconers"class="nav justify-content-around">
-            <li ><i class="fa-solid fa-earth-americas"></i></li>
-            <li ><a href="login.html"><i class="fa-regular fa-circle-user"></i></a></li>
-            <li ><i class="fa-solid fa-heart"></i></li>
-            <li ><a href="carrito.html"><i class="fa-solid fa-cart-shopping"></i></a></li>
-        </ul>   
-    </div>
-  </nav>
+{/* 
 
 <!-- <div id="frase_nav" class="container-sm justify-content-center"> -->
     <p id="frase_imaginacion">"Deja tu imaginación volar... siente la música"</p>
@@ -40,15 +9,15 @@
 
 //    --------idiomas navegador----------
 
-let espanol = ["Inicio", "Buscar", "Catálogo", "Novedades", "Ofertas"];
-let ingles = [ "Start", "Search", "Catalogue", "News", "Offers"];
-let euskera = [ "Hasi", "Bilatu", "Katalogoa", "Berria", "Eskaintzak"];
+let espanol = ["Inicio", "Buscar", "Catálogo", "Novedades", "Ofertas","Deja tu imaginación volar... siente la música"];
+let ingles = [ "Start", "Search", "Catalogue", "News", "Offers","Let your imagination fly... feel the music"];
+let euskera = [ "Hasi", "Bilatu", "Katalogoa", "Berria", "Eskaintzak","Utzi zure irudimena hegan... sentitu musika"];
 
 let array=[];
 
 array=espanol
 
-let nav = document.createElement("nav");
+//let nav = document.createElement("nav");
 
 let header= document.getElementsByTagName("header")[0]
 header.innerHTML = `
@@ -98,16 +67,10 @@ header.innerHTML = `
 </nav>`
 
 
-
 //--------idiomas frase logo----------
-
-let array_p = "Deja tu imaginación volar... siente la música";
-let ingles_p = "Let your imagination fly... feel the music";
-let euskera_p ="Utzi zure irudimena hegan... sentitu musika" ;
-
-let p= document.createElement("p");
-nav.className = "p";
-nav.innerHTML =`<p id="frase_imaginacion">"${array_p[5]}"</p>
-<!-- </div> -->`
+let p= document.createElement("p")
+p.id="frase_imaginacion"
+p.innerHTML=`${array[5]}`
+header.append(p)
 
 
